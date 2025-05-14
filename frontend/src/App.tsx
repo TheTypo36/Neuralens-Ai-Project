b5c0d6c34c53ae6ae4fb7d8c80fb6a35ff4e0d57
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-05-14 21:49:51 +0530

    landing page done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "./components/ui/button";
+import { BackgroundBeamsWithCollision } from "./components/ui/backgroundBeam";
 function App() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/product");
+  };
   return (
-    <div>
-      <h1>Hello Neuralens Ai at frontend</h1>
+    <div className="w-full h-full flex flex-col items-center justify-center bg-gradient-to-r from-yellow-300 to-red-300">
+      <BackgroundBeamsWithCollision>
+        <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+          Welcome Neuralen AI{" "}
+          <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+            <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+              <span className=""></span>
+            </div>
+            <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+              <span className="">Assignment For Internship</span>
+            </div>
+          </div>
+        </h2>
+      </BackgroundBeamsWithCollision>
+      <div>
+        <Button variant="ghost" size={"lg"} onClick={handleClick}>
+          Products
+        </Button>
+      </div>
     </div>
   );
 }
